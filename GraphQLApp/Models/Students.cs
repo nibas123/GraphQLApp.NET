3797fa5374004c8301bfd9f38baf635c37db41fa
Nibas Nazeem <nibas.nazeem@nestgroup.net>
2024-03-29 22:53:06 +0530

    Add project files.

---
+++ GraphQLApp/Models/Students.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GraphQLApp.Models
+{
+    public class Students
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string StudentName { get; set; }
+        public string Course { get; set; }
+        public int Age { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
---
+++ GraphQLApp/Models/StudentsDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace GraphQLApp.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Students> Students { get; set; }
+    }
+}
